f52f36732397df2288cb94cc3e8607904c084c6b
ionartiland <ionartiland@gmail.com>
2020-04-21 16:29:25 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    //scripts go here
+    $("#name").val("Please enter your full name");
+    $("#email").val("john@example.com");
+    $("#field-ec636cf874420f9").val("International format");
+    $("#field-6e2531eba1fbc22").val("");
+    $("input").focus(function() {
+        $(this).select();
+    });
+});
