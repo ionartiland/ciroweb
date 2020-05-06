c8cb2a85ac0f73b271a4b210badef4cae2f9b4e9
ionartiland <ionartiland@gmail.com>
2020-05-06 12:43:50 +0200

    Add the Go to top button

--- js/common.js
+++ js/common.js
@@ -1,10 +1,31 @@
-$(document).ready(function() {
-    //scripts go here
-    $("#name").val("Please enter your full name");
-    $("#email").val("john@example.com");
-    $("#field-ec636cf874420f9").val("International format");
-    $("#field-6e2531eba1fbc22").val("");
-    $("input").focus(function() {
-        $(this).select();
-    });
+//for the Go to top button
+var mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//for the contact form
+$(document).ready(function () {
+  //scripts go here
+  $('#name').val('Please enter your full name');
+  $('#email').val('john@example.com');
+  $('#field-ec636cf874420f9').val('International format');
+  $('#field-6e2531eba1fbc22').val('');
+  $('input').focus(function () {
+    $(this).select();
+  });
 });
